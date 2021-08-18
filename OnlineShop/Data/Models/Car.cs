1c13ca631f43865aafe7d5717f64a567e9a2f540
Пётр Захаров <peterzaharov@outlook.com>
2021-08-18 21:16:30 +0500

    Добавьте файлы проекта.

---
+++ OnlineShop/Data/Models/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Data.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public string Image { get; set; }
+        public ushort Price { get; set; }
+        public bool IsFavorite { get; set; }
+        public int Available { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ OnlineShop/Data/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Data.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int CategoryName { get; set; }
+        public int CategoryDescription { get; set; }
+        public List<Car> Cars { get; set; }
+    }
+}
